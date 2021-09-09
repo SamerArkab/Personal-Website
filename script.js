cbbd6c494062e63253b76a03a580478c44f459ee
SamrArkab <SamerR93@hotmail.com>
2021-09-09 13:27:40 +0300

    updates

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var mario = document.getElementById('mario');
+var flaglight = document.getElementById('flaglight');
 
 window.addEventListener('scroll', function() {
-  var value = window.scrollY;
-  mario.style.top = value * 1.2355 + 'px';
+  var value = window.scrollY; //number of pixels that the document is currently scrolled vertically
+  mario.style.top = value * 0.199 + 'vh'; //height of viewport
+
+  if (window.scrollMaxY == value)
+    flaglight.style.visibility = "visible";
+  else flaglight.style.visibility = "hidden";
 })
