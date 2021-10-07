0a2717da57d4a82cbde759d85d73fa59993fb63c
SamrArkab <SamerR93@hotmail.com>
2021-10-08 01:50:23 +0300

    lots of bootstrap adjustments......

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ var flaglight = document.getElementById('flaglight');
 
 window.addEventListener('scroll', function() {
   var value = window.scrollY; //number of pixels that the document is currently scrolled vertically
-  mario.style.top = value * 0.1396 + 'vh'; //height of viewport
+  mario.style.top = value * 0.129 + 'vh'; //height of viewport
 
   if (window.scrollMaxY == value)
     flaglight.style.visibility = "visible";
   else flaglight.style.visibility = "hidden";
 })
+
+let links = document.querySelectorAll('.nav-link');
+for(let i=0; i<links.length; i++){
+  links[i].addEventListener('click', function() { //add event listener to each nav-link element
+    for(let j=0; j<links.length; j++)
+      links[j].classList.remove('active'); //remove the active
+    this.classList.add('active'); //add active class to the clicked element
+  });
+}
