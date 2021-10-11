84dc8357c6ad62e9c81a94c2d819221eb102fafb
SamrArkab <SamerR93@hotmail.com>
2021-10-11 23:57:07 +0300

    :ambulance:

--- script.js
+++ script.js
@@ -3,27 +3,37 @@ var flaglight = document.getElementById('flaglight');
 
 window.addEventListener('scroll', function() {
   var value = window.scrollY; //number of pixels that the document is currently scrolled vertically
-  mario.style.top = value * 0.1268 + 'vh'; //height of viewport
+  mario.style.top = value * 0.1315 + 'vh'; //height of viewport
 
   if (window.scrollMaxY == value)
     flaglight.style.visibility = "visible";
   else flaglight.style.visibility = "hidden";
 })
 
-let links = document.querySelectorAll('.nav-link');
-for(let i=0; i<links.length; i++){
+var links = document.querySelectorAll('.nav-link');
+for (var i = 0; i < links.length; i++) {
   links[i].addEventListener('click', function() { //add event listener to each nav-link element
-    for(let j=0; j<links.length; j++)
+    for (var j = 0; j < links.length; j++)
       links[j].classList.remove('active'); //remove the active
     this.classList.add('active'); //add active class to the clicked element
   });
 }
 
-let navLinks = document.querySelectorAll('.nav-item')
-let menuToggle = document.getElementById('navbarSupportedContent')
-let bsCollapse = new bootstrap.Collapse(menuToggle)
+var navLinks = document.querySelectorAll('.nav-item')
+var menuToggle = document.getElementById('navbarSupportedContent')
+var bsCollapse = new bootstrap.Collapse(menuToggle)
 navLinks.forEach((l) => {
-    l.addEventListener('click', () => {
-      bsCollapse.toggle() //"click" on navbar collapse button after nav-item click
-    })
+  l.addEventListener('click', () => {
+    bsCollapse.toggle() //"click" on navbar collapse button after nav-item click
+  })
 })
+
+/*
+window.addEventListener('scroll', (e) => {
+    if(window.scrollY == 0) { // top position
+      // open the navbar
+    } else {
+      menuToggle.collapse = 'hide'; // collapse it
+    }
+});
+*/
